3c7ab74565f1a66df044dfe9513e7b93f2c6355e
0xMartijnB <martijn.boon.ext@allianz-trade.com>
2025-10-12 18:24:17 +0200

    unused

--- src/main/java/com/mb/efvdata/EfvdataApplication.java
+++ src/main/java/com/mb/efvdata/EfvdataApplication.java
@@ -2,8 +2,9 @@ package com.mb.efvdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EfvdataApplication {
 
 	public static void main(String[] args) {
